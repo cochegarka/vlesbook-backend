33c3cf206ad1e84ef51e211585a0f2803e7e4c5f
Anton Zavyalov <megadeathlightsaber@gmail.com>
2020-05-30 21:42:08 +0700

    Simplest HTTP server example

--- cmd/vlesbook/main.go
+++ cmd/vlesbook/main.go
@@ -1,11 +1,23 @@
 package main
 
 import (
-	"fmt"
-	"vlesbook/pkg/example"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+	"os"
 )
 
 func main() {
-	example.Foo()
-	fmt.Println("Hello, mister Troitsky!")
+	r := mux.NewRouter()
+	r.HandleFunc("/encrypt", func(writer http.ResponseWriter, request *http.Request) {
+		log.Println("Надо зашифровать")
+	})
+
+	r.HandleFunc("/decrypt", func(writer http.ResponseWriter, request *http.Request) {
+		log.Println("Надо расшифровать")
+	})
+
+	// TODO:
+	// Погугли "golang dotenv", разберись как работает, вынеси порт в env загружай через переменные среды (os.Env)
+	log.Fatal(http.ListenAndServe(os.Getenv("PORT"), r))
 }
