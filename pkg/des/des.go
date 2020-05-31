d74ea0b8086753c8c63514492ed1ede19fe426a6
Anton Zavyalov <megadeathlightsaber@gmail.com>
2020-05-31 21:18:18 +0700

    Reformat code, fix docs and shadow 2 methods to package level

--- pkg/des/des.go
+++ pkg/des/des.go
@@ -8,25 +8,25 @@ import (
 	"strconv"
 )
 
-//процедура дешифровки
-//вход: ключ, вектор инициализации. зашифрованный текст
-//выход: открытый текст, сообщение об ошибке
-func Decryption(key, iv, cipherText []byte) ([]byte, error) {
+// Decryption дешифрует данные
+// Входные данные: ключ, вектор инициализации. зашифрованный текст
+// Выходные данные: открытый текст, сообщение об ошибке
+func Decryption(cipherData, key, iv []byte) ([]byte, error) {
 	block, err := des.NewCipher(key)
 	if err != nil {
 		return nil, err
 	}
 	blockMode := cipher.NewCBCDecrypter(block, iv)
-	sourceData := make([]byte, len(cipherText))
-	blockMode.CryptBlocks(sourceData, cipherText)
-	plainText := PKCS5UnPadding(sourceData)
+	sourceData := make([]byte, len(cipherData))
+	blockMode.CryptBlocks(sourceData, cipherData)
+	plainText := pkcs5Unpadding(sourceData)
 	return plainText, nil
 }
 
-//процедура шифрования
-//вход: ключ, вектор инициализации. открытый текст
-//выход: шифрованный текст, ошибка
-func Encryption(key, iv, plainText []byte) ([]byte, error) {
+// Encryption расшифровывает данные
+// Вход: ключ, вектор инициализации. открытый текст
+// Выход: шифрованный текст, ошибка
+func Encryption(plainData, key, iv []byte) ([]byte, error) {
 	block, err := des.NewCipher(key) //создать новый шифр
 	if err != nil {
 		return nil, err
@@ -35,22 +35,22 @@ func Encryption(key, iv, plainText []byte) ([]byte, error) {
 	if len(iv) != blockSize {
 		return nil, errors.New("Invalid iv length (must be " + strconv.Itoa(blockSize) + ")")
 	}
-	alignedData := PKCS5Padding(plainText, blockSize) //выровнять данные
+	alignedData := pkcs5Padding(plainData, blockSize) //выровнять данные
 	blockMode := cipher.NewCBCEncrypter(block, iv)
 	encrypted := make([]byte, len(alignedData))
 	blockMode.CryptBlocks(encrypted, alignedData)
 	return encrypted, nil
 }
 
-//процедура дозаполнения текста для краткного кодирования блоками
-func PKCS5Padding(src []byte, blockSize int) []byte {
+// Процедура дозаполнения текста для кратного кодирования блоками
+func pkcs5Padding(src []byte, blockSize int) []byte {
 	padding := blockSize - len(src)%blockSize
-	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
-	return append(src, padtext...)
+	padText := bytes.Repeat([]byte{byte(padding)}, padding)
+	return append(src, padText...)
 }
 
-//процедура снятия дозаполнения
-func PKCS5UnPadding(src []byte) []byte {
+// Процедура снятия дозаполнения
+func pkcs5Unpadding(src []byte) []byte {
 	length := len(src)
 	unpadding := int(src[length-1])
 	return src[:(length - unpadding)]
