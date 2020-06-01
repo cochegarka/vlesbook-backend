3d43cd098e8c61836128b6b3f4a3d14320cbec4b
Anton Zavyalov <megadeathlightsaber@gmail.com>
2020-06-01 09:22:13 +0700

    Introduce CORS middleware

--- cmd/vlesbook/main.go
+++ cmd/vlesbook/main.go
@@ -10,8 +10,6 @@ import (
 )
 
 func main() {
-
-	//
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
@@ -20,6 +18,19 @@ func main() {
 	r.HandleFunc("/encrypt", handlers.EncryptionHandler).Methods("POST")
 	r.HandleFunc("/decrypt", handlers.DecryptionHandler).Methods("POST")
 
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), r))
+	h := corsMiddleware(r)
+
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), h))
 	//
+}
+
+// Allows CORS for given handler.
+func corsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET")
+		w.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type")
+
+		next.ServeHTTP(w, r)
+	})
 }
